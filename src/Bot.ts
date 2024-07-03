0e293c18cc08d31c0973f903a1a98a753c725039
loan <loan.courchinoux-billonnet@koesio.com>
2024-07-03 09:31:25 +0200

    feat: :construction: add first command
    not working

--- src/Bot.ts
+++ src/Bot.ts
@@ -1,9 +1,11 @@
 import { Client } from 'discord.js';
 import configurationConfig from './utils/configuration.config';
 import ready from './listeners/ready';
+import interactionCreate from './listeners/interactionCreate';
 
 console.log('Bot is starting...');
 
 const client = new Client({ intents: [] });
 ready(client);
+interactionCreate(client);
 client.login(configurationConfig().token);
